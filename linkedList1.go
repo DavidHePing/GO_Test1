3c963d43438f6f4a27591cdaa88ee6c2addc384f
Dav He <s991179@gmail.com>
2024-09-15 19:07:00 +0800

    feat: print all linked list

--- linkedList1.go
+++ linkedList1.go
@@ -12,4 +12,10 @@ func linkedList_test1() {
 	l.PushBack(3)
 
 	fmt.Println("Front", l.Front().Value, ",Back", l.Back().Value)
+
+	// Iterate through the list and print its contents.
+	for e := l.Front(); e != nil; e = e.Next() {
+		fmt.Print(e.Value, ",")
+	}
+	fmt.Println()
 }
