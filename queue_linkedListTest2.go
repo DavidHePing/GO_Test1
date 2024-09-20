576cf767304342aa47fd38252efe1799f9c1473b
Dav He <s991179@gmail.com>
2024-09-21 00:04:13 +0800

    feat: test queue by implement with linked list

--- queue_linkedListTest2.go
+++ queue_linkedListTest2.go
@@ -1,9 +1,16 @@
 package main
 
-import "container/list"
+import (
+	"container/list"
+	"fmt"
+)
 
 type LinkedListQueue struct {
 	items list.List
+}
+
+func (l *LinkedListQueue) Len() int {
+	return l.items.Len()
 }
 
 func (l *LinkedListQueue) EnQueue(num int) {
@@ -18,4 +25,22 @@ func (l *LinkedListQueue) Dequeue() int {
 	tmp := l.items.Front().Value
 	l.items.Remove(l.items.Front())
 	return tmp.(int)
+}
+
+func Fake_Queue_LinkedList_Test2() {
+	var l LinkedListQueue
+
+	l.EnQueue(1)
+	l.EnQueue(2)
+	l.EnQueue(3)
+	l.EnQueue(4)
+
+	fmt.Println("Len:", l.Len())
+
+	for l.Len() > 0 {
+		fmt.Print(l.Dequeue(), ", ")
+	}
+
+	fmt.Println("")
+	fmt.Println("Len:", l.Len())
 }
